a6ec65b7facf3a681a495f9a257c2d9c5c082ef9
kashwa <kashwa80@gmail.com>
2020-06-08 10:44:13 +0200

    Test JWT auth on MongoDB.

--- routes/api.php
+++ routes/api.php
@@ -1,24 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::group([
 
-    'middleware' => 'api',
     'prefix' => 'auth'
 
-], function ($router) {
+], function () {
 
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
@@ -27,7 +14,8 @@ Route::group([
 
 });
 
-Route::middleware('api')->get('users', function (){
+// Get Users => Authenticated only.
+Route::middleware('jwt.auth')->get('users', function (){
     return \App\User::all();
 });
 
