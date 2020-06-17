b0bafe9c89d88b272c0cfe4ad0b85c1a7aee01b2
kashwa <kashwa80@gmail.com>
2020-06-17 11:51:40 +0200

    Add scripts (Python & Java)

---
+++ public/scripts/java/MainApp.java
@@ -0,0 +1,25 @@
+package scripts.java;
+
+public class MainApp {
+	
+	public static String runLengthEncoding(String text) {
+	    String encodedString = "";
+
+	    for (int i = 0, count = 1; i < text.length(); i++) {
+	        if (i + 1 < text.length() && text.charAt(i) == text.charAt(i + 1))
+	            count++;
+	        else {
+	            encodedString = encodedString.concat(Integer.toString(count))
+	                    .concat(Character.toString(text.charAt(i))).concat(", ");
+	            count = 1;
+	        }
+	    }
+	    return encodedString;
+	}
+
+	public static void main(String[] args) {
+		String text = "aaaaaaabbccccc";
+		
+		System.out.print(runLengthEncoding(text));
+	}
+}
